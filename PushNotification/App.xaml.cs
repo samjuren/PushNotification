992930c8a91f0f906fd5c5b2fc2d93bded324a7d
Samuel Juren <sammjureen@outlook.com>
2023-12-10 22:50:16 -0300

    Add project files.

---
+++ PushNotification/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using OneSignalSDK.DotNet;
+using OneSignalSDK.DotNet.Core.Debug;
+
+namespace PushNotification
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            OneSignal.Debug.LogLevel = LogLevel.VERBOSE;
+            OneSignal.Initialize(appId: "SUA CHAVE");
+            OneSignal.Notifications.RequestPermissionAsync(fallbackToSettings: true);
+
+            MainPage = new AppShell();
+        }
+    }
+}
+
+
+//MINHA CHAVE: a059805e-f88e-4140-baf0-637d54a42ad8
---
+++ PushNotification/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using OneSignalSDK.DotNet;
+
+namespace PushNotification
+{
+    public partial class MainPage : ContentPage
+    {
+        private Guid _externalId = Guid.Parse(input: "a059805e-f88e-4140-baf0-637d54a42ad8");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void AddExternalId()
+        {
+            OneSignal.Login(externalId: _externalId.ToString());
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            AddExternalId();
+        }
+    }
+}
